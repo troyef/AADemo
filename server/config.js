718d0777524615d1c9e61c635fed097633ff4af5
tfoster <tfoster@thetus.com>
2015-09-06 15:33:38 -0500

    adding secret info for heroku deployment

---
+++ server/config.js
@@ -0,0 +1,11 @@
+
+exports.tw_config = {
+  "consumerKey": process.env.TW_CONSUMERKEY,
+  "consumerSecret": process.env.TW_CONSUMERSECRET,
+  "accessToken": process.env.TW_ACCESSTOKEN,
+  "accessTokenSecret": process.env.TW_ACCESSTOKENSECRET
+};
+
+exports.se_key = process.env.SE_KEY;
+
+exports.urlBase = 'http://tef-aa-demo.herokuapp.com/';
